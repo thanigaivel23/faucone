6e175a397125572c258780e12eb21c46b408b40f
prakash <mzukar@gmail.com>
2024-03-08 13:48:22 +0530

    configured language options

--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { RiSearchLine } from "react-icons/ri";
-import award from '../../img/6award.PNG'
+import award from '../../img/6award.PNG';
+import ReactFlagsSelect, { Us } from "react-flags-select";
+import { useTranslation } from "react-i18next";
 
 const Home = () => {
+    const { t, i18n, ready } = useTranslation();
+    const [selected, setSelected] = useState("US");
+    useEffect(() => {
+        i18n.changeLanguage(selected.toLowerCase());
+    }, [selected])
     return (
         <>
+            <Us />
             {/* nav bar */}
             <nav className='tw-bg-blue-50 tw-flex tw-items-center tw-justify-between tw-px-28 tw-py-6'>
                 <div>
@@ -41,11 +49,37 @@ const Home = () => {
                     <div>Contact us</div>
                     <div className='tw-bg-blue-200 tw-h-7 tw-w-7 tw-flex tw-items-center tw-justify-center tw-rounded-full tw-p-1 '><RiSearchLine className=' ' /></div>
 
-                    <select name="" id="" className=' tw-outline-none tw-py-1 tw-px-2'>
+                    {/* <select name="" id="" className=' tw-outline-none tw-py-1 tw-px-2'>
                         <option value="">English</option>
-                        <option value="">Tamil</option>
+                        <option value="">French</option>
                         <option value="">German</option>
-                    </select>
+                        <option value="">Spanish</option>
+                        <option value="">Mailay</option>
+                        <option value="">Dutch</option>
+                        <option value="">Indonesian</option>
+                        <option value="">Thai</option>
+                        <option value="">Chinese</option>
+                        <option value="">Japanese</option>
+                    </select> */}
+                    <ReactFlagsSelect
+                        selected={selected}
+                        onSelect={data => setSelected(data)}
+                        countries={["US", "FR", "DE", "ES", "MY", "NL", "ID", "TH", "CN", "JP"]}
+                        customLabels={
+                            {
+                                US: "English",
+                                FR: "French",
+                                DE: "German",
+                                ES: "Spanish",
+                                MY: "Malay",
+                                NL: "Dutch",
+                                ID: "Indonesian",
+                                TH: "Thai",
+                                CN: "Chinese",
+                                JP: "Japanese"
+                            }}
+                        placeholder="Select Language"
+                    />
 
                 </div>
 
@@ -67,7 +101,7 @@ const Home = () => {
 
             {/* 1st section - powering brands */}
             <section className='tw-hidden xl:tw-flex tw-justify-center tw-items-center tw-flex-col tw-mt-24'>
-                <p className='tw-tracking-[3px] tw-font-abel tw-text-gray-600 tw-text-4xl'>POWERING <span className='tw-text-[#04a1d1] tw-font-bold'>BRANDS</span></p>
+                <p className='tw-tracking-[3px] tw-font-abel tw-text-gray-600 tw-text-4xl'>{t('home.hero_title')} <span className='tw-text-[#04a1d1] tw-font-bold'>BRANDS</span></p>
                 <p className='tw-tracking-[3px] tw-font-abel tw-text-gray-600 tw-text-4xl'>CREATING <span className='tw-text-[#04a1d1] tw-font-bold'>ENTREPRENEURS</span></p>
                 <p className='tw-bg-[#04a1d1] tw-h-0.5 tw-w-10 tw-mt-2'></p>
                 <p className='tw-text-gray-600 tw-mt-4'>Faucone's mission is to create entrepreneurs, power brands, and</p>
